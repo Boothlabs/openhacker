5f291dcdd472c490633df50195efb86810fc517f
Francis Booth <Eleix@users.noreply.github.com>
2017-09-18 21:04:49 -0400

    Update serverrevenue.php

--- cron/serverrevenue.php
+++ cron/serverrevenue.php
@@ -1,4 +1,4 @@
-// Loop through the Server DB and for each server that is held by a plaeyer
+// Loop through the Server DB and for each server that is held by a player
 // Check the server health and installed software (FTP, Porn, Spam, File Sharing) 
 // Calculate a payout depending on server software stock market.
 // Then pay that amount to the user (include a transaction log to show the player did get paid)
